911f06a77d3a2f3675ad9f147e3c4f8cf84e83df
sucaya-host <sucayaputra8@gmail.com>
2026-03-02 21:24:34 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,33 @@
-const CACHE_NAME = "finproAnalytics";
-
-// GANTI "finpro" dengan nama repository kamu
-const BASE_PATH = "/sistem-keuangan-2.0/";
+const CACHE_NAME = "smartfinance-cache-v1";
 
 const FILES_TO_CACHE = [
-  BASE_PATH,
-  BASE_PATH + "index.html",
-  BASE_PATH + "manifest.json",
-  BASE_PATH + "icon-192.png",
-  BASE_PATH + "icon-512.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// Activate
 self.addEventListener("activate", event => {
   event.waitUntil(clients.claim());
 });
 
+// Fetch (Cache First Strategy)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
