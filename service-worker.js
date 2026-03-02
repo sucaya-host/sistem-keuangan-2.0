6cf4596ff56d8029f030c8d2bb4a4705bb603fb2
sucaya-host <sucayaputra8@gmail.com>
2026-03-02 21:00:47 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,27 @@
-const CACHE_NAME = "finpro-cache-v3";
+const CACHE_NAME = "finproAnalytics";
+
+// GANTI "finpro" dengan nama repository kamu
+const BASE_PATH = "/sistem-keuangan-2.0/";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  BASE_PATH,
+  BASE_PATH + "index.html",
+  BASE_PATH + "manifest.json",
+  BASE_PATH + "icon-192.png",
+  BASE_PATH + "icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate
 self.addEventListener("activate", event => {
   event.waitUntil(clients.claim());
 });
 
-// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
